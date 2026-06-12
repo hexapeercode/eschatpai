592f42f579c69c02f0ae1d6fc0164f3c0bef6858
hexapeercode <hexapeerproject@gmail.com>
2026-06-12 13:49:02 +0200

    eschatpai-lo.js

--- eschatpai-lo.js
+++ eschatpai-lo.js
@@ -1,18 +1,16 @@
 (function() {
-  console.log('=== INICIO BOTPRESS ===');
+  alert('BOTPRESS INICIADO');
   
   // Cargar Botpress
   const script1 = document.createElement('script');
   script1.src = 'https://cdn.botpress.cloud/webchat/v3.6/inject.js';
   document.head.appendChild(script1);
-  console.log('Script 1 cargado');
   
   // Esperar a que inject.js cargue, luego cargar config
   setTimeout(function() {
     const script2 = document.createElement('script');
     script2.src = 'https://files.bpcontent.cloud/2026/05/19/17/20260519173633-PCRHW6DM.js';
     document.head.appendChild(script2);
-    console.log('Script 2 cargado');
   }, 500);
   
   // Estilos
@@ -23,11 +21,9 @@
     }
   `;
   document.head.appendChild(style);
-  console.log('Estilos añadidos');
   
   // Ocultar footer (después de 3 segundos)
   setTimeout(function() {
-    console.log('Iniciando ocultación de footer');
     setInterval(function() {
       const root = Array.from(document.querySelectorAll('div')).find(el => {
         try {
@@ -41,7 +37,6 @@
         const footer = root.shadowRoot.querySelector('.bpComposerFooter');
         if (footer) {
           footer.innerHTML = '';
-          console.log('Footer ocultado');
         }
         root.style.zIndex = '999999';
       }
@@ -50,9 +45,8 @@
 })();
 
 // MENSAJE PROACTIVO - Hamburguesa
+alert('CREANDO MENSAJE');
 setTimeout(function() {
-  console.log('=== CREANDO MENSAJE ===');
-  
   const msg = document.createElement('div');
   msg.innerHTML = '👋 Tens alguna pregunta?';
   msg.style.position = 'fixed';
@@ -68,15 +62,7 @@ setTimeout(function() {
   msg.style.fontWeight = '500';
   msg.style.boxShadow = '0 2px 10px rgba(0,0,0,0.15)';
   msg.style.border = '1px solid #ddd';
-  msg.style.display = 'block';
-  msg.style.visibility = 'visible';
-  msg.style.opacity = '1';
-  
-  msg.onclick = () => {
-    console.log('Mensaje clickeado');
-    msg.remove();
-  };
-  
+  msg.onclick = () => msg.remove();
   document.body.appendChild(msg);
-  console.log('Mensaje añadido al DOM:', msg);
+  alert('MENSAJE VISIBLE');
 }, 3000);
